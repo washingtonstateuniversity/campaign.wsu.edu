5ad59054785e200c00148395516cbcd4dce059a7
jeremyfelt <jeremy.felt@gmail.com>
2015-09-10 14:38:56 -0700

    Enqueue the fullPage stylesheet for use with the script

--- functions.php
+++ functions.php
@@ -7,3 +7,11 @@ add_action( 'wp_enqueue_scripts', 'campaign_enqueue_scripts', 11 );
 function campaign_enqueue_scripts() {
 	wp_enqueue_script( 'wsu-campaign-fullpage-js', get_stylesheet_directory_uri() . '/js/jquery.fullPage.min.js', array( 'jquery' ), spine_get_script_version(), true );
 }
+
+add_action( 'spine_enqueue_styles', 'campaign_enqueue_styles', 11 );
+/**
+ * Enqueue styles other than the default style.css used by the campaign theme.
+ */
+function campaign_enqueue_styles() {
+	wp_enqueue_style( 'wsu-campaign-fullpage', get_stylesheet_directory() . '/css/jquery.fullPage.css', array(), spine_get_script_version() );
+}
